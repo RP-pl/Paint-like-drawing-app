5a5d45ffbd7057ab10850e735a110cf7ce5047db
RP-pl <68811106+RP-pl@users.noreply.github.com>
2020-09-26 19:08:31 +0200

    WPF files

---
+++ WPFAppTest/ColorDialog.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFAppTest
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Window1.xaml
+    /// </summary>
+    public partial class ColorDialog : Window
+    {
+        public Color Resault = Colors.Black;
+        public ColorDialog()
+        {
+            InitializeComponent();
+            base.Width = 200;
+            base.Height = 150;
+            for(int i=1;i<255;i+=35)
+            {
+                for (int j = 1; j < 255; j+=35)
+                {
+                    for (int k = 1; k < 255; k+=35)
+                    {
+                        StackPanel sp = new StackPanel();
+                        sp.Orientation = Orientation.Horizontal;
+                        Rectangle r = new Rectangle();
+                        r.Width = 10;
+                        r.Height = 10;
+                        r.Fill = new SolidColorBrush(Color.FromRgb((byte)i, (byte)j, (byte)k));
+                        sp.Children.Add(r);
+                        Label l = new Label();
+                        l.Content = Color.FromRgb((byte)i, (byte)j, (byte)k);
+                        sp.Children.Add(l);
+                        CB.Items.Add(sp);
+                    }
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (CB.SelectedItem != null)
+            {
+                Resault = ((SolidColorBrush)((Rectangle)((StackPanel)CB.SelectedItem).Children[0]).Fill).Color;
+            }
+            DialogResult = true;
+        }
+    }
+}
---
+++ WPFAppTest/MainWindow.xaml.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+using System.IO;
+using System.Security.Permissions;
+using Xceed.Wpf.Toolkit;
+using System.Globalization;
+
+namespace WPFAppTest
+{
+    public class HeightConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Double val = (Double)value;
+            val = val + 67;
+            return (object)val;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            Double val = (Double)value;
+            val = val - 67;
+            return (object)val;
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            canvas.Background = Brushes.White;
+            DrawingAttributes dr = new DrawingAttributes();
+            dr.Color = Colors.SpringGreen;
+            canvas.DefaultDrawingAttributes = dr;
+            Binding b = new Binding("Width");
+            b.Source = canvas;
+            b.Mode = BindingMode.TwoWay;
+            Win.SetBinding(Window.WidthProperty,b);
+            Binding h = new Binding("Height");
+            h.Source = canvas;
+            h.Converter = new HeightConverter();
+            h.Mode = BindingMode.TwoWay;
+            Win.SetBinding(Window.HeightProperty, h);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            canvas.Strokes.Clear();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.SaveFileDialog dialog = new Microsoft.Win32.SaveFileDialog();
+            dialog.Filter = "Plik|*.png";
+            dialog.ShowDialog();
+            try
+            {
+                FileStream fs = new FileStream(dialog.FileName, FileMode.Create);
+                RenderTargetBitmap rtb = new RenderTargetBitmap((int)canvas.Width, (int)canvas.Height, 96d, 96d, PixelFormats.Default);
+                rtb.Render(canvas);
+                PngBitmapEncoder encoder = new PngBitmapEncoder();
+                encoder.Frames.Add(BitmapFrame.Create(rtb));
+                encoder.Save(fs);
+                fs.Close();
+            }
+            catch (ArgumentException ex)
+            {
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.Owner = this;
+            cd.ShowDialog();
+            DrawingAttributes dr = canvas.DefaultDrawingAttributes;
+            dr.Color = cd.Resault;
+            canvas.DefaultDrawingAttributes = dr;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            StrokeDialog sd = new StrokeDialog();
+            sd.Owner = this;
+            sd.ShowDialog();
+            DrawingAttributes dr = canvas.DefaultDrawingAttributes;
+            dr.Height = sd.ResaultHeight;
+            dr.Width = sd.ResaultWidth;
+            canvas.DefaultDrawingAttributes = dr;
+        }
+    }
+}
